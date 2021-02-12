7bb2a14e58fd992ad0fb75cb80d4022221885662
Newp <mynewp@gmail.com>
2021-02-13 00:41:48 +0900

    utf8json에 대한 테스트를 외부 영향없이 돌도록

--- src/Dotnet.JsonBenchmark/Utf8JsonParser.cs
+++ src/Dotnet.JsonBenchmark/Utf8JsonParser.cs
@@ -10,11 +10,11 @@ namespace Dotnet.JsonBenchmark
         public string Name => "Utf8Json";
         public ConsoleColor BarColor => ConsoleColor.Green;
 
-        public T DeserializeFromString<T>(string json)=> JsonSerializer.Deserialize<T>(Encoding.UTF8.GetBytes(json));
+        public T DeserializeFromString<T>(string json)=> JsonSerializer.Deserialize<T>(json);
 
         public T DeserializeFromUtf8<T>(byte[] utf8json) => JsonSerializer.Deserialize<T>(utf8json);
 
-        public string SerializeToString<T>(T instance) => Encoding.UTF8.GetString(JsonSerializer.Serialize(instance));
+        public string SerializeToString<T>(T instance) => JsonSerializer.ToJsonString(instance);
 
         public byte[] SerializeToUtf8<T>(T instance) => JsonSerializer.Serialize(instance);
     }
