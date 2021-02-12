eb8de02a663739fa018d0b3b2634c2c7df37a2b4
Newp <mynewp@gmail.com>
2021-02-12 15:14:26 +0900

    IJsonParser 인터페이스 분리

---
+++ src/Dotnet.JsonBenchmark/IJsonParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using Npgg;
+
+namespace Dotnet.JsonBenchmark
+{
+    interface IJsonParser
+    {
+        string Name { get; }
+        ConsoleColor BarColor { get; }
+        string SerializeToString<T>(T instance);
+        byte[] SerializeToUtf8<T>(T instance);
+
+
+        T DeserializeFromString<T>(string json);
+        T DeserializeFromUtf8<T>(byte[] utf8json);
+
+        public ConsoleBarChartOption Check(int testCount, Action action)
+        {
+            var watch = Stopwatch.StartNew();
+
+            for (int i = 0; i < testCount; i++)
+            {
+                action();
+            }
+
+            watch.Stop();
+
+            var result = new ConsoleBarChartOption()
+            {
+                BarColor = this.BarColor,
+                Name = this.Name,
+                Value = watch.ElapsedMilliseconds
+            };
+            return result;
+        }
+    }
+}
--- src/Dotnet.JsonBenchmark/Program.cs
+++ src/Dotnet.JsonBenchmark/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using Npgg;
@@ -59,37 +58,5 @@ namespace Dotnet.JsonBenchmark
             }
         }
 
-    }
-
-    interface IJsonParser
-    {
-        string Name { get; }
-        ConsoleColor BarColor { get; }
-        string SerializeToString<T>(T instance);
-        byte[] SerializeToUtf8<T>(T instance);
-
-
-        T DeserializeFromString<T>(string json);
-        T DeserializeFromUtf8<T>(byte[] utf8json);
-
-        public ConsoleBarChartOption Check(int testCount, Action action)
-        {
-            var watch = Stopwatch.StartNew();
-
-            for (int i = 0; i < testCount; i++)
-            {
-                action();
-            }
-
-            watch.Stop();
-
-            var result = new ConsoleBarChartOption()
-            {
-                BarColor = this.BarColor,
-                Name = this.Name,
-                Value = watch.ElapsedMilliseconds
-            };
-            return result;
-        }
     }
 }
